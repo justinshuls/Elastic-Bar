56f7cd530c4dbcdf4690ed88be0b69b75bee4284
Justin Shulman <Jshulmancloud@me.com>
2013-10-22 11:15:49 -0400

    CleanUp1
    -updated animation times
    -improved responsivness
    -cleaned up code

--- elasticBar.js
+++ elasticBar.js
@@ -1,30 +1,35 @@
 //Note: line height is set to match navigation_height and navigation_min_height respectivly
 navigation_height = 100
-navigation_min_height = 60
+navigation_min_height = 40
 $(window).scroll(function(){
-if(($('body').scrollTop()>=-20) && ($('body').scrollTop()<=40) ){
+if(($('body').scrollTop()>=-40) && ($('body').scrollTop()<=(navigation_height-navigation_min_height))){
+	//controls elements dimentions on scroll
 	var s = $('body').scrollTop();
 	console.log(s);
 	$('#navigation').stop().animate({
             height:navigation_height-s,
-    },75);
-	//Elements under navigation bar fix
-	$('#nextElementFix').stop().animate({
-			paddingTop:navigation_height-s
-    },75);
+    },10);
 	$('#navigation li a').stop().animate({
 			height:navigation_height-s,
 			lineHeight:navigation_height-s,
-    },75);
-}
-if(($('body').scrollTop()>=-20) && ($('body').scrollTop()>=40) ){
+    },10);
+	//Elements under navigation bar fix
+	//Laggy? Remove this code. The over flow is has a pretty cool effect anyway.
+	$('#nextElementFix').stop().animate({
+			paddingTop:navigation_height-s
+    },10);
+} else if(($('body').scrollTop()>=-40) && ($('body').scrollTop()>=(navigation_height-navigation_min_height)) ){
+	//hold animation on fast scroll 
 	var s = $('body').scrollTop();
 	$('#navigation').stop().animate({
             height:navigation_min_height
-        },75);
+        },15);
 	$('#navigation li a').stop().animate({
 			height:navigation_min_height,
 			lineHeight:navigation_min_height,
-        },75);
+        },15);
+		$('#nextElementFix').stop().animate({
+			paddingTop:navigation_min_height-s
+    	},15);
 }
 });
